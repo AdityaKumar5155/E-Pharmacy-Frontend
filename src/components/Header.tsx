92237c43e4d2e5f536af743d000a234fbabd83de
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-20 14:56:33 +0530

    chore : corrected the tailwind installation with postcss, added the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header className="fixed top-0 right-0 left-0 h-16 bg-black">
+      <img src="logo.svg" alt="Logo" className="w-16" />
+    </header>
   )
 }
 
