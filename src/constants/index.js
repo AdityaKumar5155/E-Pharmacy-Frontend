ed55e8773ecb065d4aa8ea6171affbd293bbee1e
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-20 15:12:11 +0530

    chore : added navMenus and url

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navItems = [
+    {
+        title: 'Home',
+        url: '/'
+    },
+    {
+        title: 'Store',
+        url: '/store'
+    },
+    {
+        title: 'Contact Us',
+        url: '/contact'
+    },
+    {
+        title: 'About Us',
+        url: '/about'
+    }
+]
