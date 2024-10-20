7d9ce5bf4a34794c17a06dd6533d5fdc9aab60c0
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-20 15:04:27 +0530

    feat : added the logoSection

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,19 @@
 
+const LogoSection = () => {
+  return (
+    <>
+      <div className="flex ">
+        <img src="logo.svg" alt="Logo" className="w-8" />
+        <div className="ml-3 text-xl font-bold"><span className="text-cyan-400">E-</span><span className="text-pink-400">Pharmacy</span></div>
+      </div>
+    </>
+  )
+}
+
 const Header = () => {
   return (
-    <header className="fixed top-0 right-0 left-0 h-16 bg-black">
-      <img src="logo.svg" alt="Logo" className="w-16" />
+    <header className="sticky top-0 right-0 left-0 h-16 bg-white flex items-center px-5 shadow-md">
+      <LogoSection/>
     </header>
   )
 }
