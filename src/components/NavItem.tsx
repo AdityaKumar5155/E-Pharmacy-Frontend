7ed2b1e3e54566f7e4d806b879e238bbfafa4f57
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-20 16:20:35 +0530

    feat:  added scaling feature to navItem

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FC, useState } from 'react'
 import { navItemProps, navItemBasicProps } from "../types";
 
 const NavItemBasic: FC<navItemBasicProps> = ({ navItem, isDropped }) => {
     const { title, url, isDropdown } = navItem;
+    const location = useLocation();
+    const isCurrent = location.pathname === url;
+
     return (
-        <Link to={url} className="group relative cursor-pointer font-medium transition-all duration-300 hover:font-bold">
-            {`${title}${isDropdown ? (isDropped ? " ▲" : " ▼") : ""}`}
-            <div className=" underline z-0 border-cyan-400 absolute bottom-0 left-0 h-0.5 bg-cyan-400 underline-width-0 underline-transition group-hover:underline-width-full"></div>
+        <Link
+            to={url}
+            className={`group relative cursor-pointer font-medium transition-all duration-300 ${isCurrent ? 'text-pink-500' : 'text-cyan-400'}`}
+        >
+            <span className="inline-block transform transition-transform duration-300 group-hover:scale-105 transform-origin-bottom">
+                {`${title}${isDropdown ? (isDropped ? " ▲" : " ▼") : ""}`}
+            </span>
+            <div
+                className={`underline z-0 absolute bottom-0 left-0 h-0.5 ${isCurrent ? 'bg-cyan-400' : 'bg-pink-500'}  underline-width-0 underline-transition group-hover:underline-width-full`}
+            ></div>
         </Link>
-    )
-}
+    );
+};
+
 
 const NavItem: FC<navItemProps> = ({ navItem }) => {
     const { isDropdown, dropdownItems } = navItem;
